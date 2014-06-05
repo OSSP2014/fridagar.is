674d50fff8fa40a8465890b8dd4d8c8b24bef770
Bjarki Sorens <bjerkins8@gmail.com>
2014-06-05 23:24:25 +0000

    adding datafetcher in a seperate module

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,8 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
+    'ngRoute',
+    'angular-datafetcher'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('fridagarApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, datafetcher) {
     $scope.buttonValue = 'Yee boy';
+    datafetcher.get('http://apis.is/bus/realtime').then(function (data) {
+      console.log(data);
+    }, function (reason) {
+      console.log(reason);
+    });
   });

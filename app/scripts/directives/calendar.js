d7c46136bff89eda26eca787e416957f0c14c01b
Bjarki Sorens <bjerkins8@gmail.com>
2014-06-17 08:39:14 +0000

    beautifying with css

--- app/scripts/directives/calendar.js
+++ app/scripts/directives/calendar.js
@@ -7,23 +7,18 @@ angular.module('fridagarApp')
       link: function postLink(scope, element, attrs) {
         console.log(attrs);
         var template =
-          '<div class="clndr-controls"> \
-            <div class="clndr-previous-button">&lsaquo;</div> \
-            <div class="month"><%= month %></div> \
-            <div class="clndr-next-button">&rsaquo;</div> \
-          </div> \
-          <div class="clndr-grid"> \
-          <div class="days-of-the-week"> \
-            <% _.each(daysOfTheWeek, function(day) { %> \
-              <div class="header-day"><%= day %></div> \
-            <% }); %> \
-            <div class="days"> \
-              <% _.each(days, function(day) { %> \
-                <div class="<%= day.classes %>"><%= day.day %></div> \
+          '<div class="clndr-grid"> \
+            <div class="days-of-the-week clearfix"> \
+              <% _.each(daysOfTheWeek, function(day) { %> \
+                <div class="header-day"><%= day %></div> \
               <% }); %> \
+              <div class="days clearfix"> \
+                <% _.each(days, function(day) { %> \
+                  <div class="<%= day.classes %>"><%= day.day %></div> \
+                <% }); %> \
+              </div> \
             </div> \
-          </div> \
-        </div>';
+          </div>';
         $(element).clndr({
           template: template
         });

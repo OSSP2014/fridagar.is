52fecdf06425692071ebf77b73dcddd1eaaf2eb2
Tómas Magnússon <Kessler@tomass-air.lan>
2014-06-29 12:33:03 +0000

    removed results

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,7 +6,7 @@ angular.module('fridagarApp')
     // scope variables
     //
     var _today = moment().format('YYYY-MM-DD');
-    $scope.holidays = holidays.result;
+    $scope.holidays = holidays;
     $scope.daysToNextHoliday = holidayservice.daysToNextHoliday(_today, $scope.holidays);
     $scope.holidaysInCurrentMonth = holidayservice.countHolidaysInMonth(_today, $scope.holidays);
   });
